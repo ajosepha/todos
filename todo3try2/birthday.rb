c10a6cfb1cd59d80484c94fc5c7562bbf3e1bd4b
ajosepha <ariel.isaacs@gmail.com>
2013-11-14 11:36:24 -0500

    add todo3try2 with birthday.rb

---
+++ todo3try2/birthday.rb
@@ -0,0 +1,6 @@
+#birthday.rb
+puts "what is your birthday"
+birthday = gets.chomp
+today = "Nov 14" || "November 14" || "November 14th" || "11/13/2013" || "11-13" 
+if birthday = today
+puts "Happy Birthday!"
