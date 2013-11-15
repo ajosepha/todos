32afe40678a3619a9cf98b20fc6bbe8657016824
ajosepha <ariel.isaacs@gmail.com>
2013-11-15 09:24:25 -0500

    add count_sentances, doesn't work yet

---
+++ todo4/count_sentences.rb
@@ -0,0 +1,17 @@
+# Write a method on String called `count_sentences` that returns the number of
+# sentences in the string it is called on
+
+class String(sentance)
+  def count_sentences
+   #what can end a sentance? [ .  ! ?]
+   # create a method that counts the number of  . , ?
+   #want it to count the number of times those characters appear
+   #can turn it into an array
+   #then count the number of items in an array
+   #=~ it the match operator, which will match the string you are using
+   /\.|\?|!/
+   return number
+   end
+end
+
+
