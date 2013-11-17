51f4343f9e420c5323d45c98d2b908d4fdf7c8b2
ajosepha <ariel.isaacs@gmail.com>
2013-11-17 18:42:01 -0500

    add working V2

--- games/blackjackv2.rb
+++ games/blackjackv2.rb
@@ -18,81 +18,51 @@ def hit_me(move)
 		player_move1 << cards.sample
 		player_move1 = player_move1.reduce(:+)
 		if player_move1 < 21 
-			puts "your score is #{player_move1}, want to try again?"
+			puts "your score is #{player_move1}, want to try again? Y or N"
+			answer = gets.chomp.upcase
+			if answer == "Y"
+				player_move1 = [player_move1]
+				player_move1 << cards.sample
+				player_move1 = player_move1.reduce(:+)
+				puts "Your new score is #{player_move1}"
+			elsif anwer == "N"
+				puts "Ok, game over. Your scoare is #{player_move1}"
+			else
+				puts "not a valid answer"
+			end				
 		elsif player_move1 == 21
 			puts "YOU WIN!"
 		else		
 		puts "your score is #{player_move1}. you lost"
 		end
 end
-hit_me(player_move)
 
-puts "Y to hit. N to stay"
+puts "Do you want to hit or stay. Y/N"
 response = gets.chomp.upcase
-	if response = "Y"
-		player_move1 = player_move
+	if response == "Y"
 		hit_me(player_move)
-	else
-		puts "game over"
-	end	
+	elsif response == "N"
+		puts "Game over, your final score was #{player_move}"
+	end
+				
 
 
 
 
-
-
-
-#def hit_or_stay(move)
-	#if player_move == 21
-	#puts "Game over, you win!"
-	#else
-	#puts "Your first total is #{player_move}. Type y to hit or n to end."
-	#answer = gets.chomp.downcase
-		#if answer == "n"
-		#puts "Ok. Your final score is #{player_move}. Game over."	
-		#elsif answer == "y"
-		#this will need to be a method defined above this question.  Lets try it..
-		#puts "let's play some more..."
-		#player_move1 = []
-		#player_move1 << player_move
-		#player_move1 << cards.sample
-		#player_move1 = player_move1.reduce(:+)
-			#if player_move1 > 21
-			#puts "You got #{player_move1}. Boo you lose!"
-			#elsif player_move1 < 21	
-			#puts "You're new score is #{player_move1}."
-			#else
-			#puts "you got 21! WIN WIN WIN!"	
-			#end			
-	#end
-#end
-#end	
-#hit_or_stay(player_move)
-
-#if player_move1 > 21
+#if player_move > 21
 	#puts "Game over, you lost!"
-#else
-	#puts "Your second total is #{player_move1}. Type y to hit or n to end."
-	#answer = gets.chomp.downcase
-	#if answer == "y"
-		#this will need to be a method defined above this question.  Lets try it..
-		#puts "let's play  more..."
-		#player_move2 = []
-		#player_move2 << player_move1
-		#player_move2 << cards.sample
-		#player_move2 = player_move2.reduce(:+)
-		#if player_move2 > 21
-			#puts "Booo hoo you lose"
-		#elsif player_move2 < 21	
-			#puts "You're new score is #{player_move2}. Close but no cigar."
-		#else
-			#puts "you got 21! WIN WIN WIN!"	
-		#end	
-	#elsif anser == "n"
-		#puts "playing it safe is not always a winning strategy."	
-	#else
-		#puts "please type in y or n"		
-	#end
-#end	
-
-#for refactoring
+#elsif player_move < 21
+	#puts "Please put Y if you would like to get hit, or N to stay."
+		#response = gets.chomp.upcase
+			#if response == "Y"
+				#puts "OK. good choice"
+				#hit_me(player_move)
+			#elsif response == "N"
+				#puts "Fine. Game over. Your score was #{player_move}"
+			#else
+				#puts "please put yes or no, those are your options"
+				#response = gets.chomp.upcase
+			#end
+#else 
+	#puts "21! We have a Winner!"
+#end								
