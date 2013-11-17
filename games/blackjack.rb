d1663485bc81175d886aaabd5cd5547791c83912
ajosepha <ariel.isaacs@gmail.com>
2013-11-17 08:53:24 -0500

    add v w/o methods

--- games/blackjack.rb
+++ games/blackjack.rb
@@ -33,8 +33,40 @@ player_move << cards.sample
 #puts player_move
 #sum the values of the array together
 #got this from the ruby documentation. Not sure how it works, but it does :)
-player_move.reduce(:+)
+player_move = player_move.reduce(:+)
 puts player_move
+if player_move == 21
+	puts "Game over, you win!"
+else
+	puts "Your first total is #{player_move}. Type y to hit or n to end."
+	answer = gets.chomp.downcase
+	if answer == "n"
+		puts "Ok. Your final score is #{player_move}. Game over."	
+	elsif answer == "y"
+		#this will need to be a method defined above this question.  Lets try it..
+		puts "let's play  more..."
+		player_move1 = []
+		player_move1 << player_move
+		player_move1 << cards.sample
+		player_move1 = player_move1.reduce(:+)
+		puts "You're new score is #{player_move1}"
+	else
+		puts "please type in y or n"		
+	end
+end	
+
+#for refactoring
+def check_win(move)
+		if move == 21
+			puts "congrats, you are the winner!"
+		else
+			puts "Do you want to pick another card?"
+		end
+end
+
+
+
+
 
 
 
