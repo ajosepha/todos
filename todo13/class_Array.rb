bdac7c03a83b3576e5cd18f74a507e906d891e8d
ajosepha <ariel.isaacs@gmail.com>
2013-12-03 10:13:59 -0500

    try more regex, still doesn't work

--- todo13/class_Array.rb
+++ todo13/class_Array.rb
@@ -34,11 +34,42 @@ require 'ap'
 
   def version_sort(file)
     #split strings into an array from before the -
+    part_1 = []
     file.each do |version|
-      part_1 = version.split(/-/)
-      ap part_1[1]
-       
+      part_1 << version.split(/-/)[1]  
     end 
+    part2 = part_1.sort_by{|x| x.scan(/\d/)}
+    part3 = part2.sort_by{|x| x.scan(/\[0-9\]\D*./)}
+    part4 = part3.sort_by{|x| x.scan(/(?<=\.)/)
+    #part 4 does nothing...
+    #now want it to sort by when the period comes
+    ap part4
+    #sort by 1.
+
+    # [ 0] "1.ext",
+    # [ 1] "10.ext",
+    # [ 2] "100.ext",
+    # [ 3] "10.1.ext",
+    # [ 4] "1.10.2.ext",
+    # [ 5] "1.11.ext",
+    # [ 6] "1.3.ext",
+    # [ 7] "13.ext",
+    # [ 8] "1.50.ext",
+    # [ 9] "1.8.7.ext",
+    # [10] "1.9.3.ext",
+    # [11] "2.0a.ext",
+    # [12] "2.0b.ext",
+    # [13] "2.0.ext",
+    # [14] "2.0.0.ext",
+    # [15] "2.007.ext",
+    # [16] "2.01.ext",
+    # [17] "2.01a.ext",
+    # [18] "2.0.1.ext",
+    # [19] "2.012b.ext",
+    # [20] "2.1.ext",
+    # [21] "25.ext",
+    # [22] "6.ext"
+
   end
   version_sort(filenames)
 
