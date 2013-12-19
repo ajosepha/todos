6ca49033514b9cd54ba02d73a3fcfb7825643937
ajosepha <ariel.isaacs@gmail.com>
2013-12-18 23:09:18 -0500

    add with logic

--- todo22/lib/model.rb
+++ todo22/lib/model.rb
@@ -11,8 +11,15 @@ class Birthday
     def run
         if tu_bshvat
         elsif purim
+        elsif passover
+        elsif shavout
+        elsif tish_baav
+        elsif rosh_hashanah
+        elsif yom_kippur
+        elsif sukkot
+        elsif hannukah
         else
-            "Lo, it's not a Jewish holiday today!"
+          "No, it's not a Jewish holiday today!"
         end
     end
 
@@ -31,95 +38,101 @@ class Birthday
     end
 
     def passover
-        if @today.day == 14 && @today.month == 04
+        if @today.day == 14 && @today.month == 4
              "It's the first night of Passover, watch out for that Manishevitz"
-        elsif @today.day == 15 && @today.month == 04
+        elsif @today.day == 15 && @today.month == 4
              "It's passover, enjoy your gluten-free life!"
-        elsif @today.day == 16 && @today.month == 04
+        elsif @today.day == 16 && @today.month == 4
              "It's passover, enjoy your gluten-free life!"
-        elsif @today.day == 17 && @today.month == 04
+        elsif @today.day == 17 && @today.month == 4
              "It's passover, enjoy your gluten-free life!"
-        elsif @today.day == 18 && @today.month == 04
+        elsif @today.day == 18 && @today.month == 4
              "It's passover, enjoy your gluten-free life!"
-        elsif @today.day == 19 && @today.month == 04
+        elsif @today.day == 19 && @today.month == 4
              "It's passover, enjoy your gluten-free life!"
-        elsif @today.day == 20 && @today.month == 04
+        elsif @today.day == 20 && @today.month == 4
              "It's passover, enjoy your gluten-free life!"
-        elsif @today.day == 21 && @today.month == 04
+        elsif @today.day == 21 && @today.month == 4
              "It's passover, enjoy your gluten-free life!"
-        elsif @today.day == 22 && @today.month == 04
+        elsif @today.day == 22 && @today.month == 4
             "Congrats! you made it to the last night of Passover! Go have a pizza, you deserve it."
         end
     end
 
-    # def shavout
-    #    if @today.day == 03 && @today.month == 06
-    #     "It's the start of Shavout, you probably don't care unless you're in Israel"
-    #   elsif @today.day == 04 && @today.month == 06
-    #     "Still Shavout"
-    #   elsif @today.day == 05 && @today.month == 06
-    #     "Shavout ends tonight!"
-    # end
+    def shavout
+       if @today.day == 3 && @today.month == 6
+        "It's the start of Shavout, you probably don't care unless you're in Israel"
+      elsif @today.day == 4 && @today.month == 6
+        "Still Shavout"
+      elsif @today.day == 5 && @today.month == 6
+        "Shavout ends tonight!"
+      end
+    end
 
-    # def rosh_hashanah
-    #   if @today.day == 24 && @today.month == 09
-    #     "Grab some apples and honey, Rosh Hashanah starts tonight"
-    #   elsif @today.day == 25 && @today.month == 09
-    #     "Have some honeycake, it's Rosh Hashanah."
-    #   elsif @today.day == 26 && @today.month == 09
-    #     "Finish off that honeycake before it goes stale, it's the last night of Rosh Hashanh"
-    #   end
-    # end
+    def tish_baav
+      if @today.day == 4 && @today.month == 8
+        "It's erev Tish B'Av"
+      elsif @today.day == 5 && @today.month == 8
+        "It's Tish B'Av"
+      end
+    end
 
-    # def yom_kippur
-    #   if @today.day == 03 && @today.month == 10
-    #     "Hope you ate a good dinner, Yom Kippur starts tonight"
-    #   elsif @today.day == 04 && @today.month == 10
-    #     "Repent."
-    #   end
-    # end
+    def rosh_hashanah
+      if @today.day == 24 && @today.month == 9
+        "Grab some apples and honey, Rosh Hashanah starts tonight"
+      elsif @today.day == 25 && @today.month == 9
+        "Have some honeycake, it's Rosh Hashanah."
+      elsif @today.day == 26 && @today.month == 9
+        "Finish off that honeycake before it goes stale, it's the last night of Rosh Hashanh"
+      end
+    end
 
-    # def sukkot
-    #   if @today.day == 08 && @today.month == 10
-    #     "it's Sukkot! Go to the sukkah"
-    #   elsif @today.day == 09 && @today.month == 10
-    #     "Dinner in the great outdoors, its Sukkot!"
-    #   elsif @today.day == 10 && @today.month == 10
-    #     "Dinner in the great outdoors, its Sukkot!"
-    #   elsif @today.day == 11 && @today.month == 10
-    #     "Dinner in the great outdoors, its Sukkot!"
-    #   elsif @today.day == 12 && @today.month == 10
-    #     "Dinner in the great outdoors, its Sukkot!"
-    #   elsif @today.day == 13 && @today.month == 10
-    #     "Dinner in the great outdoors, its Sukkot!"
-    #   elsif @today.day == 14 && @today.month == 10
-    #     "Dinner in the great outdoors, its Sukkot!"
-    #   elsif @today.day == 15 && @today.month == 10
-    #     "Dinner in the great outdoors, its Sukkot!"
-    #   end
-    # end
+    def yom_kippur
+      if @today.day == 3 && @today.month == 10
+        "Hope you ate a good dinner, Yom Kippur starts tonight"
+      elsif @today.day == 4 && @today.month == 10
+        "Repent."
+      end
+    end
 
-    # def hannukah
-    #   if @today.day == 16 && @today.month == 12
-    #     "Presents! Hannukah starts tonight!"
-    #   elsif @today.day == 17 && @today.month == 12
-    #     "Second night of hannukah, have a latka!"
-    #   elsif @today.day == 18 && @today.month == 12
-    #     "Third night of Hannukah"
-    #   elsif @today.day == 19 && @today.month == 12
-    #     "Fourth night of Hannukah, half way there!"
-    #   elsif @today.day == 20 && @today.month == 12
-    #     "Fifth night of Hannukah"
-    #   elsif @today.day == 21 && @today.month == 12
-    #     "Sixth night of Hannukah!"
-    #   elsif @today.day == 22 && @today.month == 12
-    #     "Seventh night of Hannukah"
-    #   elsif @today.day  == 23 && @today.month == 12
-    #     "Last night of Hannukah!"
-    #   end
-    # end
+    def sukkot
+      if @today.day == 8 && @today.month == 10
+        "it's Sukkot! Go to the sukkah"
+      elsif @today.day == 9 && @today.month == 10
+        "Dinner in the great outdoors, its Sukkot!"
+      elsif @today.day == 10 && @today.month == 10
+        "Dinner in the great outdoors, its Sukkot!"
+      elsif @today.day == 11 && @today.month == 10
+        "Dinner in the great outdoors, its Sukkot!"
+      elsif @today.day == 12 && @today.month == 10
+        "Dinner in the great outdoors, its Sukkot!"
+      elsif @today.day == 13 && @today.month == 10
+        "Dinner in the great outdoors, its Sukkot!"
+      elsif @today.day == 14 && @today.month == 10
+        "Dinner in the great outdoors, its Sukkot!"
+      elsif @today.day == 15 && @today.month == 10
+        "Dinner in the great outdoors, its Sukkot!"
+      end
+    end
 
-
-
+    def hannukah
+      if @today.day == 16 && @today.month == 12 && @today.year ==2014
+        "Presents! Hannukah starts tonight!"
+      elsif @today.day == 17 && @today.month == 12 && @today.year ==2014
+        "Second night of Hannukah, have a latka!"
+      elsif @today.day == 18 && @today.month == 12 && @today.year ==2014
+        "Third night of Hannukah"
+      elsif @today.day == 19 && @today.month == 12 && @today.year ==2014
+        "Fourth night of Hannukah, half way there!"
+      elsif @today.day == 20 && @today.month == 12 && @today.year ==2014
+        "Fifth night of Hannukah"
+      elsif @today.day == 21 && @today.month == 12 && @today.year ==2014
+        "Sixth night of Hannukah!"
+      elsif @today.day == 22 && @today.month == 12 && @today.year ==2014
+        "Seventh night of Hannukah"
+      elsif @today.day  == 23 && @today.month == 12 && @today.year ==2014
+        "The end of Hannukah!"
+      end
+    end
 
 end
