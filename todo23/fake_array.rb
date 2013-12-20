61470ba4eb2a12f4f5799cdb25dd1ec0105b4ab2
ajosepha <ariel.isaacs@gmail.com>
2013-12-20 09:36:43 -0500

    add todo 23 and 24

---
+++ todo23/fake_array.rb
@@ -0,0 +1,20 @@
+
+class FakeArray
+ 
+  def each
+
+    
+  end
+ 
+  def first
+    element(0)
+    
+  end
+ 
+  def []()
+    
+  end
+ 
+end
+
+#want the array to return an object with elements
