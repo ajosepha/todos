a7134161a258d97e41f2fb25324f6478bf516785
ajosepha <ariel.isaacs@gmail.com>
2014-01-21 09:27:10 -0500

    add fizzbuzz in JS

---
+++ todo41/spec/fizzBuzzSpec.js
@@ -0,0 +1,14 @@
+describe( "#fizBuzz", function (){
+  it("returns fizzbuzz if i is divisible by 15", function(){
+    expect(fizzbuzz(15)).toBe("fizzbuzz");
+  });
+
+  it("returns fizz if i is divisible by 3", function(){
+    expect(fizzbuzz(9)).toBe("fizz");
+  });
+
+  it("returns buzz if i divisible by 5", function(){
+    expect(fizzbuzz(20)).toBe("buzz");
+  });
+});
+
---
+++ todo41/src/fizzBuzz.js
@@ -0,0 +1,15 @@
+function fizzBuzz(i){
+  for(i = 1; i < 100; i ++){
+    if(i % 15 === 0){
+      return("fizzbuzz")
+    };
+    else if(i % 3 === 0){
+      return("fizz")
+    };
+    else if (i % 5 === 0){
+      return("buzz")
+    }
+    else
+      return(i)
+  }
+}
