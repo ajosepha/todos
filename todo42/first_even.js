cc62260608fd4bc1766303b5a350238d3329446d
ajosepha <ariel.isaacs@gmail.com>
2014-01-27 09:24:41 -0500

    add todo42

--- todo42/first_even.js
+++ todo42/first_even.js
@@ -1,10 +1,3 @@
-# prereqs: arrays, methods, conditional logic
-# Write a method that returns the first even element in a array of integers
-# This is an old todo we did in Ruby, please rewrite your answer in javascript
- 
-def first_even(items)
-  // code goes here
-end
 
 var num = []
 var first_even = function(array){
